b1d367f23dc5619b360a36995e5c5acf19f76fe5
Khushank Raj Mahawan <03929802016@vips.edu>
2020-02-17 05:47:12 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ puts"Welcome to Tic Tac Toe!"
 puts"Where would you like to go?"
 input=gets.strip
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-index=input+1
+index=0
 def input_to_index(array, index, input)
   array[index] = input
 end
